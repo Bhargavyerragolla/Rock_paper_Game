74b95c3f3499e9ff288a3adc7d59a2e32a562f20
Bhargavyerragolla <bhargavsai8887@gmail.com>
2024-01-27 12:43:24 +0530

    update

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        int []arr={2,3,4,4,5};
+        for(int ar:arr) {
+            System.out.println(ar);
+        }
+        }
+}
